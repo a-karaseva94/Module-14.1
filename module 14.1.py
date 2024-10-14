57b8f6c67b79d8909649834b5f1a0c1ba91d3ffa
a-karaseva94 <a-karaseva94@mail.ru>
2024-10-14 10:09:36 +0300

    Add files via upload

--- module 14.1.py
+++ module 14.1.py
@@ -15,31 +15,31 @@ balance INTEGER NOT NULL
 
 cursor.execute("CREATE INDEX IF NOT EXISTS idx_email ON Users(email)")
 
-# for i in range(1, 11):
-#     cursor.execute("INSERT INTO Users (username, email, age, balance) "
-#                    "VALUES (?, ?, ?, ?)", (f"User{i}", f"example{i}@gmail.com", f"{i * 10}", "1000"))
+for i in range(1, 11):
+    cursor.execute("INSERT INTO Users (username, email, age, balance) "
+                   "VALUES (?, ?, ?, ?)", (f"User{i}", f"example{i}@gmail.com", f"{i * 10}", "1000"))
 
 
 # Обновление balance у каждой 2ой записи начиная с 1ой на 500:
 
-# for i in range(1, 11):
-#     if i % 2 == 0:
-#         pass
-#     else:
-#         cursor.execute("UPDATE Users SET balance = ? WHERE username = ?", (500, f"User{i}"))
+for i in range(1, 11):
+    if i % 2 == 0:
+        pass
+    else:
+        cursor.execute("UPDATE Users SET balance = ? WHERE username = ?", (500, f"User{i}"))
 
 
-# Удаление каждой 3ей записи в таблице начиная с 1ой:
+#Удаление каждой 3ей записи в таблице начиная с 1ой:
 
-# cursor.execute("DELETE FROM Users WHERE id IN (1, 4, 7, 10)")
+cursor.execute("DELETE FROM Users WHERE id % 3 = 1")
 
 
-# выбор всех записей
+#выбор всех записей
 
-# cursor.execute("SELECT * FROM Users WHERE age != ?", (60,))
-# users = cursor.fetchall()
-# for data in users:
-#     print(f" Имя: {data[1]} | Почта: {data[2]} | Возраст: {data[3]} | Баланс: {data[4]}")
+cursor.execute("SELECT * FROM Users WHERE age != ?", (60,))
+users = cursor.fetchall()
+for data in users:
+    print(f" Имя: {data[1]} | Почта: {data[2]} | Возраст: {data[3]} | Баланс: {data[4]}")
 
 
 connection.commit()
